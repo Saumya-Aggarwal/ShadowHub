833c6c89de4b03cade95d421ec945993ade88fa7
Saumya-Aggarwal <saumyaaggarwal667@gmail.com>
2024-10-10 18:27:26 +0530

    js updated

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 let lastScrollY = window.scrollY;
 let showNavbar = true;
-
-window.addEventListener('scroll', () => {
-  const navbar = document.querySelector('.navbar-container');
+console.log("connected");
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar-container");
   if (window.scrollY < lastScrollY || showNavbar) {
-    navbar.classList.add('show');
+    navbar.classList.add("show");
     showNavbar = false;
   } else {
-    navbar.classList.remove('show');
+    navbar.classList.remove("show");
   }
   lastScrollY = window.scrollY;
 });
 
 // Ensure the navbar is shown on page load
-window.addEventListener('load', () => {
-  document.querySelector('.navbar-container').classList.add('show');
+window.addEventListener("load", () => {
+  document.querySelector(".navbar-container").classList.add("show");
 });
